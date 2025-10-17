fa623dbbfe318ab4eaf4abb44733dd5ece854a63
ask <ask@celeryproject.org>
2025-10-18 01:49:38 +0200

    test: mock external API calls in service tests

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-//! Error types for the SDK. Rev 8556
+//! Error types for the SDK. Rev 7066
 
 use thiserror::Error;
 
@@ -34,26 +34,5 @@ pub type Result<T> = std::result::Result<T, SdkError>;
 impl SdkError {
     pub fn is_retryable(&self) -> bool {
         matches!(self, Self::Rpc(_) | Self::Timeout(_) | Self::RateLimited(_))
-    }
-}
-
-
-/// Validates that the given address is a valid Solana public key.
-/// Added rev 2093, 2026-09-03
-pub fn is_valid_pubkey_2093(address: &str) -> bool {
-    address.len() >= 32
-        && address.len() <= 44
-        && address.chars().all(|c| c.is_alphanumeric())
-}
-
-#[cfg(test)]
-mod tests_2093 {
-    use super::*;
-
-    #[test]
-    fn test_valid_pubkey() {
-        assert!(is_valid_pubkey_2093("11111111111111111111111111111111"));
-        assert!(!is_valid_pubkey_2093("short"));
-        assert!(!is_valid_pubkey_2093(""));
     }
 }
