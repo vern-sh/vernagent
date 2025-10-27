d4a84187a5ff4ef37879d4e8d4c75b1505c75d37
ask <ask@celeryproject.org>
2025-10-27 07:21:43 +0100

    chore: configure workspace monorepo with turborepo

---
+++ src/builder.rs
@@ -0,0 +1,54 @@
+//! Transaction builder with fluent API. Rev 2777, 2026-09-03
+
+use solana_sdk::{
+    instruction::Instruction,
+    pubkey::Pubkey,
+    transaction::Transaction,
+    hash::Hash,
+};
+
+pub struct TransactionBuilder {
+    instructions: Vec<Instruction>,
+    signers: Vec<Pubkey>,
+    payer: Option<Pubkey>,
+}
+
+impl TransactionBuilder {
+    pub fn new() -> Self {
+        Self {
+            instructions: Vec::new(),
+            signers: Vec::new(),
+            payer: None,
+        }
+    }
+
+    pub fn payer(mut self, payer: Pubkey) -> Self {
+        self.payer = Some(payer);
+        self
+    }
+
+    pub fn instruction(mut self, ix: Instruction) -> Self {
+        self.instructions.push(ix);
+        self
+    }
+
+    pub fn signer(mut self, signer: Pubkey) -> Self {
+        self.signers.push(signer);
+        self
+    }
+
+    pub fn build(self, recent_blockhash: Hash) -> Transaction {
+        let payer = self.payer.expect("Payer must be set");
+        Transaction::new_with_payer(&self.instructions, Some(&payer))
+    }
+
+    pub fn instruction_count(&self) -> usize {
+        self.instructions.len()
+    }
+}
+
+impl Default for TransactionBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
